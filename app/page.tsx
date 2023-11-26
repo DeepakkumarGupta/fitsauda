db9b2f4764281776d498c0954a1948eaaf8b4777
DeepakkumarGupta <dg90471@gmail.com>
2023-11-27 00:13:16 +0530

    improved the responsiveness for the page

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,36 @@ import { FaInstagram } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 ">
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 sm:p-8 lg:p-16">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-2 pt-4 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get ready for something amazing from Fitsauda!
         </p>
       </div>
 
-      <div className="mb-8 text-4xl font-bold text-white bg-gradient-to-r from-slate-900 to-purple-900 p-4 rounded-full">
+      <div className="mb-4 text-2xl sm:text-3xl lg:text-4xl font-bold text-white bg-gradient-to-r from-slate-900 to-purple-900 p-2 sm:p-4 lg:p-6 rounded-full">
         fitsauda.com
       </div>
 
-      <div className="mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0">
-        <h1 className="text-4xl font-bold mb-4">Coming Soon!</h1>
-        <p className="text-lg mb-8">
-          We are working on something awesome for you all fitness freeks stay
-          tuned!!..
+      <div className="mb-8 text-center lg:max-w-5xl lg:w-full lg:mb-0">
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-2 sm:mb-4">
+          Coming Soon!
+        </h1>
+        <p className="text-base sm:text-lg lg:text-xl mb-4 sm:mb-8">
+          We are working on something awesome for you all fitness enthusiasts!
+          Stay tuned!
         </p>
-        <div className="flex items-center justify-center mb-8">
-          <p className="text-lg mr-2">Follow us on Instagram for updates:</p>
+        <div className="flex flex-col items-center justify-center mb-8">
+          <p className="text-base sm:text-lg lg:text-xl mb-2">
+            Follow us on Instagram for updates:
+          </p>
           <a
             href="https://www.instagram.com/fitsaudaa/"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-2 underline text-blue-300"
           >
-            <FaInstagram size={24} />
+            <FaInstagram size={20} />
             @fitsaudaa
           </a>
         </div>
